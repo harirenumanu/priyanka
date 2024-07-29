46599dd3e2a5d4ae558bd02f3ecd7b237b8514da
harirenumanu <90784065+harirenumanu@users.noreply.github.com>
2024-07-29 14:36:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "example" {
-ami = "ami-0c55b159cbfafe1f0"
+ami = "ami-0427090fd1714168b"
 instance_type = "t2.micro"
 }
 output "ip" {
