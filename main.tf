a72913d28ed088bf659d116066da9499485f4dee
harirenumanu <90784065+harirenumanu@users.noreply.github.com>
2024-11-21 12:34:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "example" {
 ami = "ami-012967cc5a8c9f891"
-instance_type = "t2.medium"
+instance_type = var.instance_type
 }
 output "ip" {
 value = aws_instance.example.public_ip
